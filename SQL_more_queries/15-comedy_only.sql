4fdb360d4bf8da7fa0cd42bdff1ff8c58bd18eeb
Nhialmakol <n.makol@alustudent.com>
2023-08-02 04:10:18 -0700

    15-comedy_only.sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,17 @@
--- Uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
-SELECT `name` FROM `tv_genres`
-	LEFT JOIN `tv_show_genres` ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
-	LEFT JOIN `tv_shows` ON `tv_show_genres`.`show_id` = `tv_shows`.`id`
-	WHERE `tv_shows`.`title` = 'Dexter'
-	GROUP BY `name`
-	ORDER BY `name` ASC;
+-- Lists all comedy shows in the database hbtn_0d_tvshows.
+-- Records are ordered by descending show title.
+SELECT t.`title`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       INNER JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE g.`name` = "Comedy"
+ ORDER BY t.`title`;
+
+
+
+
+
+
